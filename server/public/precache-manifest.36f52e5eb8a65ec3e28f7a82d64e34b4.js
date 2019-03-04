4d029af0e039c67d24e99dca6dce5e7bd371f0d8
AldoAbdn <1701183@rgu.ac.uk>
2019-03-04 22:30:46 +0000

    Including current front end public folder

---
+++ server/public/precache-manifest.36f52e5eb8a65ec3e28f7a82d64e34b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "19584cea0ee2646a9a1c",
+    "url": "/static/js/main.19584cea.chunk.js"
+  },
+  {
+    "revision": "00f7b2e3123537f745cf",
+    "url": "/static/js/1.00f7b2e3.chunk.js"
+  },
+  {
+    "revision": "19584cea0ee2646a9a1c",
+    "url": "/static/css/main.486addd9.chunk.css"
+  },
+  {
+    "revision": "00f7b2e3123537f745cf",
+    "url": "/static/css/1.9f71be7d.chunk.css"
+  },
+  {
+    "revision": "f48a5300b26d790fe44102f883689c5c",
+    "url": "/index.html"
+  }
+];
---
+++ server/public/precache-manifest.64ad0ff44d61adf8440e5b5f847109e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dba4a6e05b9d938126b9",
+    "url": "/static/js/main.dba4a6e0.chunk.js"
+  },
+  {
+    "revision": "e81a35a581c5cd7f9b86",
+    "url": "/static/js/1.e81a35a5.chunk.js"
+  },
+  {
+    "revision": "dba4a6e05b9d938126b9",
+    "url": "/static/css/main.25ec737b.chunk.css"
+  },
+  {
+    "revision": "330db0918525d6c25063742100e52aaa",
+    "url": "/index.html"
+  }
+];
