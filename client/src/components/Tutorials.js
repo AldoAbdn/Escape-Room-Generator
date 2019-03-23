6f21a6f9e3487045fbac20f7e823a95135c6de97
Alistair Quinn <1701183@rgu.ac.uk>
2019-03-23 08:48:33 +0000

    Updating environment variable name

--- client/src/components/Tutorials.js
+++ client/src/components/Tutorials.js
@@ -7,7 +7,7 @@ class Tutorials extends Component {
             <YouTubePlaylist
                 width={'85%'}
                 height={'100%'}
-                api_key={process.env.youtubeAPIKey}
+                api_key={process.env.YOUTUBE_API_KEY}
                 playlist_id="PLWRRwFEmEuDKX5sobzWeb50xD14cpfkk3"
                 scrolling={true}
             />
