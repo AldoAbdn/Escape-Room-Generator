b76490dcd906c70e454d503b67927e754f6927bf
Alistair Quinn <1701183@rgu.ac.uk>
2019-03-23 18:22:11 +0000

    Adding environment variables

--- client/src/components/Tutorials.js
+++ client/src/components/Tutorials.js
@@ -7,7 +7,7 @@ class Tutorials extends Component {
             <YouTubePlaylist
                 width={'85%'}
                 height={'100%'}
-                api_key={process.env.YOUTUBE_API_KEY}
+                api_key={process.env.REACT_APP_YOUTUBE_API_KEY}
                 playlist_id="PLWRRwFEmEuDKX5sobzWeb50xD14cpfkk3"
                 scrolling={true}
             />
