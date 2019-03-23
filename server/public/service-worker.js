18e81b639bfa8aa57d19c18c0fb3971ae9c1f5b7
Alistair Quinn <1701183@rgu.ac.uk>
2019-03-23 08:51:43 +0000

    Adding latest build to public folder

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5df3a45fc73bdc4e418b401c72bd8e63.js"
+  "/precache-manifest.f0ff83106ced338f53524e9a2227698a.js"
 );
 
 workbox.clientsClaim();
