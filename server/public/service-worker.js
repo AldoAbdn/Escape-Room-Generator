e50d3ee9bfa4630cea314c8d9a073c48a41838d3
Alistair Quinn <1701183@rgu.ac.uk>
2019-03-25 11:00:10 +0000

    Latest build

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.89c81ea9750108a47ca814759cf3a8ad.js"
+  "/precache-manifest.7b08dcf96af4577500b7c6742cdb57a9.js"
 );
 
 workbox.clientsClaim();
