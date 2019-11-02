e0c08dde090685c820bcf0d5f35e6e4925be4a61
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-11-02 20:13:16 +0000

    testing

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ const logger = require('./logger');
 const app = require('./app');
 const hostname = app.get('host');
 const port = process.env.PORT || app.get('port');
-const server = app.listen(port, hostname);
+const server = app.listen(port);
 
 process.on('unhandledRejection', (reason, p) =>
   logger.error('Unhandled Rejection at: Promise ', p, reason)
