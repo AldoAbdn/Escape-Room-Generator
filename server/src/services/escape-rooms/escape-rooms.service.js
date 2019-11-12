098f6e9c655907fb2e618f260c5fef4ce6236531
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-11-12 22:25:41 +0000

    Fixing escape rooms service

--- server/src/services/escape-rooms/escape-rooms.service.js
+++ server/src/services/escape-rooms/escape-rooms.service.js
@@ -8,10 +8,10 @@ module.exports = function (app) {
   };
 
   // Initialize our service with any options it requires
-  app.use('/escaperooms', new EscapeRooms(options, app));
+  app.use('/escape-rooms', new EscapeRooms(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('escaperooms');
+  const service = app.service('escape-rooms');
 
   service.hooks(hooks);
 };
