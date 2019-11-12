061c00f92a4d379538996f58e5537511a5383b30
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-11-12 20:52:00 +0000

    update to imports

--- server/src/services/authmanagement/authmanagement.hooks.js
+++ server/src/services/authmanagement/authmanagement.hooks.js
@@ -1,4 +1,4 @@
-const auth = require('@feathersjs/authentication').hooks;
+const auth = require('@feathersjs/authentication');
 const commonHooks = require('feathers-hooks-common');
 const isAction = (...args) => hook => args.includes(hook.data.action);
 
