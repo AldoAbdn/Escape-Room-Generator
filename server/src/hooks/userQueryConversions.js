c6c4ccf3fb3b6672f79952a2b1ff6327a54a404d
Alistair Quinn <35927172+AldoAbdn@users.noreply.github.com>
2019-11-13 09:11:35 +0000

    Update userQueryConversions.js
    fogot to update query

--- server/src/hooks/userQueryConversions.js
+++ server/src/hooks/userQueryConversions.js
@@ -4,9 +4,10 @@ module.exports = function (options = {}) {
     return async context => {
       const { query = {} } = context.params;
       
-      if(query._id){
+      if(query._id)
         query._id = new ObjectID(query._id); 
-      }
+      
+      context.params.query = query;
   
       // Best practice: hooks should always return the context
       return context;
