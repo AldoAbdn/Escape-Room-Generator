192e13fd0255751f1f907e35662bce6ee170efa0
Alistair Quinn <1701183@rgu.ac.uk>
2019-11-23 20:15:14 +0000

    Trying to fix #79

--- server/src/app.js
+++ server/src/app.js
@@ -32,6 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
 // Host the public folder
 app.use('/', express.static(app.get('public')));
+app.use('*', express.static(app.get('public')));
 
 // Set up Plugins and providers
 app.configure(express.rest());
