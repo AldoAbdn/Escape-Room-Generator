ddb36642f40b520ef590e8e90f82b2881971de4d
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-12-22 15:17:03 +0000

    trying to only allow verified accounts to login

--- client/src/components/BusinessLogic.js
+++ client/src/components/BusinessLogic.js
@@ -154,7 +154,9 @@ class BusinessLogic extends Component {
                 <Route path="/signup" render={(routeProps) => (<Signup signUp={this.signUp}/>)}/>
                 <Route path="/about" component={About}/>
                 <Route path="/tutorials" component={Tutorials}/>
+                <Route path="/verify" component={Verify}/>
                 <Route path="/verify/:token" render={(routeProps) => (<Verify token={routeProps.match.params.token} verify={this.verify}/>)}/>
+                <Route path="/reset" component={Reset}/>
                 <Route path="/reset/:token" componet={(routeProps) => (<Reset token={routeProps.match.params.token} reset={this.reset}/>)}/>
                 <Route component={NotFound}/>
             </Switch> 
--- server/src/authentication.js
+++ server/src/authentication.js
@@ -1,6 +1,8 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication');
 const { LocalStrategy } = require('@feathersjs/authentication-local');
 const { expressOauth } = require('@feathersjs/authentication-oauth');
+const verifyHooks = require('feathers-authentication-management').hooks;
+const auth = require('@feathersjs/authentication').hooks;
 
 module.exports = app => {
   const authentication = new AuthenticationService(app);
@@ -10,4 +12,26 @@ module.exports = app => {
 
   app.use('/authentication', authentication);
   app.configure(expressOauth());
+
+  app.service("authentication").hooks({
+    before: {
+      create: [
+        auth.authenticate(config.strategies),
+        verifyHooks.isVerified() // !!! Add the isVerified hook before authentication
+      ],
+      remove: [
+        auth.authenticate('jwt')
+      ]
+    },
+    after: {
+      create: [
+        context => {
+          // Add the user to the result response
+          context.result.user = context.params.user;
+          // Don't expose sensitive information.
+          delete context.result.user.password;
+        }
+      ]
+    }
+  })
 };
