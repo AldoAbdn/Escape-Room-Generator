95ba74d29dae3e633970b03908209c8ed1de1fc4
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-12-22 14:32:20 +0000

    Fixing reference error

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -1,11 +1,11 @@
 const users = require('./users/users.service.js');
 const escapeRooms = require('./escape-rooms/escape-rooms.service.js');
 const mailer = require('./mailer/mailer.service.js');
-const authmanagement = require('./authmanagement/auth-management.service.js');
+const authManagement = require('./auth-management/auth-management.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(escapeRooms);
   app.configure(mailer);
-  app.configure(authmanagement);
+  app.configure(authManagement);
 };
