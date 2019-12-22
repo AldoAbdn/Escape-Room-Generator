add62568049117d5c8e5f2550145347f0db91dac
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-12-22 14:48:18 +0000

    fixing path to auth management

--- server/src/services/auth-management/auth-management.service.js
+++ server/src/services/auth-management/auth-management.service.js
@@ -4,10 +4,10 @@ const notifier = require('./notifier');
 const authManagement = require('feathers-authentication-management');
 module.exports = function (app) {
   // Initialize our service 
-  app.configure(authManagement(notifier(app)));
+  app.configure(authManagement({path:"auth-management",notifier:notifier(app)}));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('authManagement');
+  const service = app.service('auth-management');
 
   service.hooks(hooks);
 };
