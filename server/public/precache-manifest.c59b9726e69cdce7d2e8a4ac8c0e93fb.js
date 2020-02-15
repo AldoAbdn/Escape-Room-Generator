633123494920c848677123e99df558aa8e526015
Alistair Quinn <1701183@rgu.ac.uk>
2020-02-15 18:50:46 +0000

    Latest build

---
+++ server/public/precache-manifest.c59b9726e69cdce7d2e8a4ac8c0e93fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d83ec7bb20a5a92efb764646391cef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7ae043dc5bfcd749c49",
+    "url": "/static/css/2.5c5f4c00.chunk.css"
+  },
+  {
+    "revision": "a0980b2a2552d5205fe0",
+    "url": "/static/css/main.1a7391fe.chunk.css"
+  },
+  {
+    "revision": "f7ae043dc5bfcd749c49",
+    "url": "/static/js/2.25aa64b7.chunk.js"
+  },
+  {
+    "revision": "a0980b2a2552d5205fe0",
+    "url": "/static/js/main.ee417b10.chunk.js"
+  },
+  {
+    "revision": "101b7beba6a3d0265e4e",
+    "url": "/static/js/runtime-main.2ec80145.js"
+  }
+]);
