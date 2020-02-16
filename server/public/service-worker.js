c525427f42f483db01233b42e9689bd36ea8efae
Alistair Quinn <1701183@rgu.ac.uk>
2020-02-16 19:47:47 +0000

    Trying to fix designer

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9260102552ed1874adc6a5fc6498a01.js"
+  "/precache-manifest.1895af3c1e288900b3015c858682fc07.js"
 );
 
 self.addEventListener('message', (event) => {
