8330a71b02fc3e513b705e86274fd20d527623ea
Alistair <alistairaldo@hotmail.co.uk>
2022-06-28 21:01:41 +0100

    Fixes reactstrap imports

--- client/src/components/ListCreator.js
+++ client/src/components/ListCreator.js
@@ -1,5 +1,5 @@
 import React, {Component}  from 'react';
-import {Row, Col, Button, Input, InputGroup, InputGroupAddon, InputGroupText} from 'reactstrap';
+import {Row, Col, Button, Input, InputGroup, InputGroupText} from 'reactstrap';
 import PropTypes from 'prop-types';
 
 /**
@@ -62,11 +62,9 @@ class ListCreator extends Component {
         return (
             <InputGroup key={index}>
                 <Input type="text" id={index} onChange={this.handleChange}/>
-                <InputGroupAddon addonType="append">
-                    <InputGroupText onClick={this.removeItem(index)}>
-                        delete
-                    </InputGroupText>
-                </InputGroupAddon>
+                <InputGroupText onClick={this.removeItem(index)}>
+                    delete
+                </InputGroupText>
             </InputGroup>
         )
     }
