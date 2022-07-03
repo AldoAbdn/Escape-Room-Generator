6de6dbf6a9257de3fc9c94493b850ac1c2fc4fb0
Alistair <alistairaldo@hotmail.co.uk>
2022-07-03 19:36:05 +0100

    Adds background music to runner. Need to add url to details of escape room

--- client/src/components/EscapeRoomRunner.js
+++ client/src/components/EscapeRoomRunner.js
@@ -12,6 +12,8 @@ class EscapeRoomRunner extends Component {
     constructor(props){
         super();
         this.state = {paused:true, time:props.time * 60}; // Change time from minutes to seconds
+        this.music = new Audio(props.music);
+        this.music.loop = true;
         this.hintSound = new Audio(props.hint);
     }
 
@@ -19,10 +21,14 @@ class EscapeRoomRunner extends Component {
      * Handles Timer Toggle
      */
     handleTimerToggle = ()=>{
-        if(this.state.paused)
+        if(this.state.paused){
             this.timerInterval = setInterval(this.timer, 1000);
-        else 
+            this.music.play();
+        }
+        else {
             clearInterval(this.timerInterval);
+            this.music.pause();
+        }
         this.setState({paused:!this.state.paused});
     }
 
@@ -43,6 +49,13 @@ class EscapeRoomRunner extends Component {
         this.hintSound.play();
     }
 
+    /**M
+     * Handles Timer Toggle
+     */
+    handleMusicMute = ()=>{
+        this.music.muted = !this.music.muted;
+    }
+    
     /**
      * React lifecycle method 
      * Renders layout
@@ -76,11 +89,12 @@ class EscapeRoomRunner extends Component {
                 <Row style={{height:"20%", fontSize:"2rem"}}>
                     <Col className="col text-center">
                         <textarea id="hint" style={{width:"100%", height:"100%"}}
-                        defaultValue="Welcome to the escape room runner! Use this space to type hints to your players. You can use chromecast or other screen sharing options to display this timer. To start the timer press 'SHIFT+SPACEBAR', to play a hint sound press 'CTRL+SPACEBAR'. REMEMBER TO CLEAR THIS TEXT BOX BEFORE STARTING. Enjoy your game!"/>
+                        defaultValue="Welcome to the escape room runner! Use this space to type hints to your players. You can use chromecast or other screen sharing options to display this timer. To start the timer press 'SHIFT+SPACEBAR', to play a hint sound press 'CTRL+SPACEBAR' and to mute music press 'SHIFT+M'. REMEMBER TO CLEAR THIS TEXT BOX BEFORE STARTING. Enjoy your game!"/>
                     </Col>
                 </Row>
                 <KeyboardEventHandler handleKeys={['shift+space']} onKeyEvent={(key,e)=>this.handleTimerToggle()}/>
                 <KeyboardEventHandler handleKeys={['ctrl+space']} onKeyEvent={(key,e)=>this.handleHintSound()}/>
+                <KeyboardEventHandler handleKeys={['shift+m']} onKeyEvent={(key,e)=>this.handleMusicMute()}/>
             </Container>
         )
     }
@@ -90,6 +104,7 @@ EscapeRoomRunner.propTypes = {
     name: PropTypes.string,
     time: PropTypes.number,
     background: PropTypes.string,
+    music: PropTypes.string,
     hint: PropTypes.string,
 }
 
